76a8607f31ec8ea5dcd4adc7c5759d989c2df011
DouglasWWolf <douglaswolf@yahoo.com>
2023-12-05 23:07:26 -0800

    No commit message

--- example/api.sh
+++ example/api.sh
@@ -1,3 +1,21 @@
+#==============================================================================
+# AXI register definitions
+#==============================================================================
+          REG_CTRL=0x1004
+        REG_STATUS=0x1004
+       REG_LOAD_F0=0x1008
+        REG_COUNT0=0x1008
+       REG_LOAD_F1=0x100C
+        REG_COUNT1=0x100C
+         REG_START=0x1010
+REG_CYCLES_PER_PKT=0x1014
+REG_PKTS_PER_FRAME=0x1018
+         REG_VALUE=0x1040
+REG_PKTS_PER_GROUP=0x202C
+REG_BYTES_PER_USEC=0x2030
+   REG_METACOMMAND=0x2040
+#==============================================================================
+
 
 #==============================================================================
 # This calls the local copy of pcireg
@@ -25,6 +43,90 @@ read_reg()
 
     # Hand the value to the caller
     echo $value
+}
+#==============================================================================
+
+
+#==============================================================================
+# Displays 1 if bitstream is loaded, otherwise displays "1"
+#==============================================================================
+is_bitstream_loaded()
+{
+    reg=$(read_reg $REG_LOAD_F0)
+    test $reg -ne $((0xFFFFFFFF)) && echo "1" || echo "0"
+}
+#==============================================================================
+
+
+#==============================================================================
+# Loads the bitstream into the FPGA
+#
+# Returns 0 on success, non-zero on failure
+#==============================================================================
+load_bitstream()
+{
+    sudo ./load_bitstream -hot_reset $1 1>&2
+    return $?
+}
+#==============================================================================
+
+
+
+#==============================================================================
+# Define the geometry of a data frame
+#
+# $1 = Number of bytes per packet (must be a power of 2: 64 <= value <= 8192)
+# $2 = Number of packets per frame
+#==============================================================================
+define_frame()
+{
+    # Define the number of data-cycles per packet
+    pcireg $REG_CYCLES_PER_PKT $(($1 / 64))
+
+    # Define the number of packets per data frame
+    pcireg $REG_PKTS_PER_FRAME $2
+}
+#==============================================================================
+
+
+#==============================================================================
+# Define the number of packets in a single burst of the ping-ponger
+#==============================================================================
+set_ping_pong_group()
+{
+    pcireg $REG_PKTS_PER_GROUP $1
+}
+#==============================================================================
+
+
+#==============================================================================
+# Gets and displays the number of packets in a ping-ponger burst
+#==============================================================================
+get_ping_pong_group()
+{
+    read_reg $REG_PKTS_PER_GROUP
+}
+#==============================================================================
+
+
+#==============================================================================
+# Set the maximum output bandwidth in bytes per microsecond
+#
+# The rate should be evenly divisible by 64
+#==============================================================================
+set_rate_limit()
+{
+    pcireg $REG_BYTES_PER_USEC $1
+}
+#==============================================================================
+
+
+#==============================================================================
+# Gets and displays the maximum output bandwidth in bytes per microsecond
+#==============================================================================
+get_rate_limit()
+{
+    read_reg $REG_BYTES_PER_USEC
 }
 #==============================================================================
 
--- example/example.sh
+++ example/example.sh
@@ -1,1 +1,43 @@
+# Load our API into our current shell instance
+source api.sh
 
+# By default, we won't load the bitstream
+need_bitstream=0;
+
+# Parse the command line
+while (( "$#" )); do
+    if [ $1 == "-force" ]; then
+        need_bitstream=1
+        shift
+   else
+      echo "Invalid command line switch: $1"
+      exit 1
+   fi
+done
+
+# Is the bitstream not yet loaded?
+test $(is_bitstream_loaded) -eq 0 && need_bitstream=1
+
+# If we need to load the bitstream into the FPGA, make it so
+if [ $need_bitstream -eq 1 ]; then
+    echo "Loading bitstream..."
+    load_bitstream sidewinder_bc_emu.bit
+    test $? -eq 0 || exit 1
+    echo "Bitstream loaded"
+fi
+
+# Here, we should halt the process just in case
+#halt_output
+
+# Set the output data rate in bytes-per-microsecond. 
+set_rate_limit 12288
+
+# Our packets are 4K each, and there are 1024 packets per frame
+define_frame 4096 1024
+
+# Set the number of packets in a packet-burst on each QSFP
+set_ping_pong_group 1
+
+#
+# define FD ring addr and size, MC ring addr and size, FC_addr
+#
